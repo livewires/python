49f6ba3130635d3c8503392b2b5151e549a4e172
blob8108 <blob8108@gmail.com>
2013-09-25 13:03:46 +0100

    Switch to setuptools

--- module/livewires/__init__.py
+++ module/livewires/__init__.py
@@ -27,5 +27,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+__version__ = '2.1'
+
 from beginners import *
 # That was well worth the effort of the licence, wasn't it? :-)
--- module/setup.py
+++ module/setup.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-import sys
-
-# Default to installing if no commands are given
-if len (sys.argv) == 1:
-	script_args = ["install"]
-else:
-	script_args = sys.argv [1:]
+import glob
+from setuptools import setup
+import re
 
 
+version = re.search("__version__ = '([^']+)'",
+                    open('livewires/__init__.py').read()).group(1)
 
 setup (name = "LiveWires",
-       version = "2.1",
+       version = version,
+       install_requires = ['pygame'],
        description = "LiveWires package provides resources for people learning Python. It is intended for use with the LiveWires Python Course",
        author = "Richard Crook, Gareth McCaughan, Paul Wright, Rhodri James, Neil Turton",
        author_email = "python@livewires.org.uk",
        url = "http://www.livewires.org.uk/python/",
        packages = ['livewires'],
-       script_args = script_args
       )
 
