ae5a934cdf4e08fce8cb5a1834b55bc1e1854a4f
Daniel Watkins <daniel@daniel-watkins.co.uk>
2014-08-06 15:45:21 +0100

    Fix formatting in setup.py.

--- module/setup.py
+++ module/setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import glob
 from setuptools import setup
 import re
@@ -8,13 +7,16 @@ import re
 version = re.search("__version__ = '([^']+)'",
                     open('livewires/__init__.py').read()).group(1)
 
-setup (name = "LiveWires",
-       version = version,
-       install_requires = ['pygame'],
-       description = "LiveWires package provides resources for people learning Python. It is intended for use with the LiveWires Python Course",
-       author = "Richard Crook, Gareth McCaughan, Paul Wright, Rhodri James, Neil Turton",
-       author_email = "python@livewires.org.uk",
-       url = "http://www.livewires.org.uk/python/",
-       packages = ['livewires'],
-      )
-
+setup(
+    name="LiveWires",
+    version=version,
+    install_requires=['pygame'],
+    description="LiveWires package provides resources for people learning"
+                " Python. It is intended for use with the LiveWires Python"
+                " Course",
+    author="Richard Crook, Gareth McCaughan, Paul Wright, Rhodri James,"
+           " Neil Turton",
+    author_email="python@livewires.org.uk",
+    url="http://www.livewires.org.uk/python/",
+    packages=['livewires'],
+)
